6e01c2b4b0d9df78bb66fba91de319d766fda64f
Reid <rlj0713@gmail.com>
2021-06-12 15:27:51 -0400

    added user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
+    validates :name, presence: true
 
     has_many :created_notes, foreign_key: :creator_id, class_name: 'Note'
     has_many :notes_about_teachers, foreign_key: :target_id, class_name: 'Note'
