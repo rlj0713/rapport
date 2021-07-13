8964d12d588b926ba590e42c10296c00a28e8072
Reid <rlj0713@gmail.com>
2021-07-13 18:25:32 -0400

    fixed sorting bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
     def index
         @user = current_user
         if @user.coach
-            @users_at_permission_level = User.all.where(coach: false, school_id: @user.school_id)
+            @users_at_permission_level = User.all.where(coach: false, school_id: @user.school_id).sort_by_name
         else
             @users_at_permission_level = @user
         end
